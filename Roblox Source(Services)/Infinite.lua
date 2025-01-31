fba80395498930aae040715b232846d427c3e527
BBD31 <daydayda7@gmail.com>
2025-01-31 22:25:39 +0200

    Infinite.lua

---
+++ Roblox Source(Services)/Infinite.lua
@@ -0,0 +1,12 @@
+while wait(0.5) do -- you can change 0.5 to any time for repeat
+    --loop function 
+end 
+local time = (0.1) -- you too can change time for repeat
+while wait(time) do 
+    --loop function 
+end
+local isRun = true --for toggle change "true" to callback
+while isRun do
+	--loop function 
+  wait(0.5)
+end
